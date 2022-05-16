316b5b9ed9ea407c252374e0edfc99d83a90ac36
Yozi Berlianda <60866348+zii99@users.noreply.github.com>
2022-05-16 21:43:44 +0700

    Add files via upload

---
+++ frontend/check.php
@@ -0,0 +1,97 @@
+<section class="roberto-about-area section-padding-100-0">
+    <!-- Hotel Search Form Area -->
+    <div class="hotel-search-form-area">
+        <div class="container-fluid">
+            <div class="hotel-search-form">
+                <form action="#" method="post">
+                    <div class="row justify-content-between align-items-end">
+                        <div class="col-6 col-md-2 col-lg-3">
+                            <label for="checkIn">Check In</label>
+                            <input type="date" class="form-control" id="checkIn" name="checkin-date">
+                        </div>
+                        <div class="col-6 col-md-2 col-lg-3">
+                            <label for="checkOut">Check Out</label>
+                            <input type="date" class="form-control" id="checkOut" name="checkout-date">
+                        </div>
+                        <div class="col-4 col-md-1">
+                            <label for="room">Room</label>
+                            <select name="room" id="room" class="form-control">
+                                <option value="01">01</option>
+                                <option value="02">02</option>
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                            </select>
+                        </div>
+                        <div class="col-4 col-md-1">
+                            <label for="adults">Adult</label>
+                            <select name="adults" id="adults" class="form-control">
+                                <option value="01">01</option>
+                                <option value="02">02</option>
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                            </select>
+                        </div>
+                        <div class="col-4 col-md-2 col-lg-1">
+                            <label for="children">Children</label>
+                            <select name="children" id="children" class="form-control">
+                                <option value="01">01</option>
+                                <option value="02">02</option>
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                            </select>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <button type="submit" class="form-control btn roberto-btn w-100">Check
+                                Availability</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-100">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-6">
+                <!-- Section Heading -->
+                <div class="section-heading wow fadeInUp" data-wow-delay="100ms">
+                    <h6>About Us</h6>
+                    <h2>Welcome to <br>Roberto Hotel Luxury</h2>
+                </div>
+                <div class="about-us-content mb-100">
+                    <h5 class="wow fadeInUp" data-wow-delay="300ms">With over 340 hotels worldwide, NH Hotel
+                        Group offers a wide variety of hotels catering for a perfect stay no matter where your
+                        destination.</h5>
+                    <p class="wow fadeInUp" data-wow-delay="400ms">Manager: <span>Michen Taylor</span></p>
+                    <img src="../img/core-img/signature.png" alt="" class="wow fadeInUp" data-wow-delay="500ms">
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-6">
+                <div class="about-us-thumbnail mb-100 wow fadeInUp" data-wow-delay="700ms">
+                    <div class="row no-gutters">
+                        <div class="col-6">
+                            <div class="single-thumb">
+                                <img src="../img/bg-img/13.jpg" alt="">
+                            </div>
+                            <div class="single-thumb">
+                                <img src="../img/bg-img/14.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="single-thumb">
+                                <img src="img/bg-img/15.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ frontend/header.php
@@ -0,0 +1,63 @@
+<header class="header-area">
+    <!-- Search Form -->
+    <div class="search-form d-flex align-items-center">
+        <div class="container">
+            <form action="index.html" method="get">
+                <input type="search" name="search-form-input" id="searchFormInput" placeholder="Type your keyword ...">
+                <button type="submit"><i class="icon_search"></i></button>
+            </form>
+        </div>
+    </div>
+
+    <!-- Top Header Area Start -->
+    <div class="top-header-area">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-6">
+                    <div class="top-header-content">
+                        <a href="#"><i class="icon_phone"></i> <span>(123) 456-789-1230</span></a>
+                        <a href="#"><i class="icon_mail"></i> <span>info.colorlib@gmail.com</span></a>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="top-header-content">
+                        <!-- Top Social Area -->
+                        <div class="top-social-area ml-auto">
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Top Header Area End -->
+
+    <!-- Main Header Start -->
+    <div class="main-header-area">
+        <div class="classy-nav-container breakpoint-off">
+            <div class="container">
+                <!-- Classy Menu -->
+                <nav class="classy-navbar justify-content-between" id="robertoNav">
+
+                    <!-- Logo -->
+                    <a class="nav-brand" href="index.html"><img src="../img/core-img/logo.png" alt=""></a>
+
+                    <!-- Navbar Toggler -->
+                    <div class="classy-navbar-toggler">
+                        <span class="navbarToggler"><span></span><span></span><span></span></span>
+                    </div>
+
+                    <!-- Menu -->
+                    <?php include_once 'frontend/menu.php'; ?>
+
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
